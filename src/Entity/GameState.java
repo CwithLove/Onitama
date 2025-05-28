1cfc249d5ca1d299ca2ef345e929aa87de4f3ceb
CwithLove <cwithlove@TCloud>
2025-05-29 01:00:06 +0200

    Setup code skeleton

---
+++ src/Entity/GameState.java
@@ -0,0 +1,8 @@
+package Entity;
+
+public class GameState {
+    public void startGame() {
+        // Initialize game state
+        System.out.println("Game started.");
+    }
+}
---
+++ src/Onitama.java
@@ -0,0 +1,11 @@
+import Entity.GameState;
+
+public class Onitama {
+    public static void main(String[] args) {
+        // GamePanel gamePanel = new GamePanel();
+
+        GameState gameState = new GameState();
+        gameState.startGame();
+
+    }
+}
